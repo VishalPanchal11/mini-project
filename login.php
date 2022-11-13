8b394cd053f88f2004793bc76759e0a8b2085cd0
shrutiiiiin <shrutinaik2317@gmail.com>
2022-11-13 13:04:01 +0530

    kkkj

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+
+$uname = $_POST['username'];
+$pass1 = $_POST['password1'];
+$pass2 = $_POST['password2'];
+$mobile = $_POST['mobile'];
+
+$conn = mysqli ('loalhost' , 'root' , '' , 'ecomm');
+
+?>
+
+
+
